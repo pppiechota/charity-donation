00cd1d1f9c77e63d3f7c1b9359ff815df6c5eeae
Piotr Piechota <piotr_piechota@poczta.onet.pl>
2020-02-19 23:24:49 +0100

    Add three entity classes

---
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -0,0 +1,26 @@
+package pl.coderslab.charity.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Category {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/coderslab/charity/entity/Donation.java
@@ -0,0 +1,4 @@
+package pl.coderslab.charity.entity;
+
+public class Donation {
+}
