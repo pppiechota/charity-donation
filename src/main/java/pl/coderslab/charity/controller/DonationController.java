ffcbfa0723c04808dbccdd7955c9419ab1d72b98
Piotr Piechota <piotr_piechota@poczta.onet.pl>
2020-02-21 22:31:08 +0100

    Add new controller

---
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -0,0 +1,14 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DonationController {
+
+    @RequestMapping("/donation")
+    public String homeAction(Model model) {
+        return "form";
+    }
+}
